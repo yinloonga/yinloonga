c6ca0175b3f51399de08438e6fe097dd91d02ce7
yinloonga <72967324+yinloonga@users.noreply.github.com>
2023-08-05 23:27:03 +0800

    Update index.js

--- index.js
+++ index.js
@@ -19,9 +19,7 @@ const readme = `\
 ### Hi there 👋
 | Year progress | Top Langs | WakaTime (One Month) |
 | --- | --- | --- |
-| <div>⏳ Year progress: <br> ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} % <br> <br>⏰ Updated on ${beijingTime} <br>![Progress Bar CI](https://github.com/yinloonga/yinloonga/actions/workflows/main.yml/badge.svg)</div> | 
-![Top Langs](https://github-readme-stats-one-bice.vercel.app/api/top-langs/?username=yinloonga&layout=compact&theme=dark&role=OWNER,ORGANIZATION_MEMBER,COLLABORATOR) | 
-<img src="https://wakatime.com/share/@yinloonga/f920780c-f365-4860-b2d0-04bf2840a3f5.svg" width="80%" height="80%" /> |\
+| <div>⏳ Year progress: <br> ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} % <br> <br>⏰ Updated on ${beijingTime} <br>![Progress Bar CI](https://github.com/yinloonga/yinloonga/actions/workflows/main.yml/badge.svg)</div> | ![Top Langs](https://github-readme-stats-one-bice.vercel.app/api/top-langs/?username=yinloonga&layout=compact&theme=dark&role=OWNER,ORGANIZATION_MEMBER,COLLABORATOR) | <img src="https://wakatime.com/share/@yinloonga/f920780c-f365-4860-b2d0-04bf2840a3f5.svg" width="80%" height="80%" /> |\
 `
 
 console.log(readme)
