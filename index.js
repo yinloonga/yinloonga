c811d00f8fb4ef31b6a79b21bc352aba2abfee04
yinloonga <72967324+yinloonga@users.noreply.github.com>
2023-08-05 15:07:13 +0800

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const readme = `\
 ⏳ Year progress ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} %
 
 ---
-⏰ Updated on ${new Date().toUTCString()}\
+⏰ Updated on ${new Date().toUTCString()}
 `
 
 console.log(readme)
