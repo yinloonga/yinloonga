a57cae5c4907ebafad0c1e1a61c0300d456b258b
yinloonga <72967324+yinloonga@users.noreply.github.com>
2023-08-05 16:45:46 +0800

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const readme = `\
 ⏳ Year progress ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} %
 ---
 ⏰ Updated on ${new Date().toUTCString()}
-![Progress Bar CI](https://github.com/yinloonga/yinloonga/actions/workflows/main.yml/badge.svg)
-\`
+![Progress Bar CI](https://github.com/yinloonga/yinloonga/actions/workflows/main.yml/badge.svg)\
+`
 
 console.log(readme)
